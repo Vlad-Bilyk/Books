dcb08f88c98f4c95933fd95001cc4c1d26328a2f
Vladyslav Bilyk <bilyk.vlad16@gmail.com>
2025-09-11 18:14:19 +0300

    Add models

---
+++ Books.Core/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿namespace Books.Core.Models;
+
+public class Book
+{
+    public Guid Id { get; set; }
+    public required string Title { get; set; }
+    public int Pages { get; set; }
+    public Guid GenreId { get; set; }
+    public Guid AuthorId { get; set; }
+    public Guid PublisherID { get; set; }
+    public DateTime ReleaseDate { get; set; }
+
+    public Genre Genre { get; set; } = null!;
+    public Author Author { get; set; } = null!;
+    public Publishier Publishier { get; set; } = null!;
+}
---
+++ Books.Core/Models/Genre.cs
@@ -0,0 +1,9 @@
+﻿namespace Books.Core.Models;
+
+public class Genre
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+
+    public ICollection<Book> Books { get; } = [];
+}
